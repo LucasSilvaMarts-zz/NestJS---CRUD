3111bd4088465674d6d25ec79f1aece0c265bfff
Lucas Silva <martssilvapro@gmail.com>
2021-11-10 16:44:02 -0300

    configura .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://Nest:nest2121@nest.8nftg.mongodb.net/nest?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL),
   ],
   controllers: [],
   providers: [],
